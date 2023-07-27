05d3bd0916617f596ded455d44050437509e1d16
nikrassel <nikrassel@mail.ru>
2023-07-27 11:22:31 +0300

    first visual template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sorting from "./components/Sorting";
+import PositionBlock from "./components/PositionBlock";
 
 function App() {
+  const dishArray = [
+    { title: "Набор 'Плотный обед'", price: 1500, image: "/img/set_1.jpg" },
+    { title: "Набор 'Ассортимент'", price: 1000, image: "/img/set_2.jpg" },
+    { title: "Филадельфия", price: 600, image: "/img/set_3.jpg" },
+  ];
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sorting />
+          </div>
+          <h2 className="content__title">Все меню</h2>
+          <div className="content__items">
+            {dishArray.map((elem) => (
+              <PositionBlock
+                title={elem.title}
+                price={elem.price}
+                imageSource={elem.image}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">Все</li>
+        <li>С лососем</li>
+        <li>С угрём</li>
+        <li>Вегетарианские</li>
+        <li>Запечёные</li>
+        <li>Наборы</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
